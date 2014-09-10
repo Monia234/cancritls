dc0d7c0e0c16e1bc50ab9ca7fb4a389360210cf1
Masahiro Kanai <cani.m.61st@gmail.com>
2014-09-10 17:44:01 +0900

    expand tab

--- Hapmap/merge_hapmapIII.sh
+++ Hapmap/merge_hapmapIII.sh
@@ -10,11 +10,11 @@ for f in `find *.ped`
 do
     file=${f%.ped}
     plink --file $file --make-bed --out $file
-	mv $file.fam $file.fam.old
-	cut -d' ' -f1-5 $file.fam.old | gawk -v i=$i '{print $0, i}' > $file.fam  
+    mv $file.fam $file.fam.old
+    cut -d' ' -f1-5 $file.fam.old | gawk -v i=$i '{print $0, i}' > $file.fam  
     echo $file >> mergelist.txt
-	echo $file | gawk -v i=$i 'OFS="\t"{print gensub(/hapmap3_r3_b36_fwd\.([A-Z]+)\.qc\.poly/,"\\1",""), i}' >> population.txt
-	i=`expr $i + 10`
+    echo $file | gawk -v i=$i 'OFS="\t"{print gensub(/hapmap3_r3_b36_fwd\.([A-Z]+)\.qc\.poly/,"\\1",""), i}' >> population.txt
+    i=`expr $i + 10`
 done
 
 plink --merge-list mergelist.txt --make-bed --out hapmap_r3_b36_fwd.ALL.qc.poly
