4030c3967557cdc011ae13288b9c70b0b1e6e88c
Masahiro Kanai <mkanai.brc@tmd.ac.jp>
2014-09-16 16:10:58 +0900

    bug fix caused by lazy eval

--- admixture/Rccmatch.r
+++ admixture/Rccmatch.r
@@ -1,22 +1,32 @@
-Rccmatch <- function(admixture, fam, n, case.string = "2") {
+Rccmatch <- function(x, fam, n, case.string = "2") {
     require(proxy)
     require(Rcpp)
     sourceCpp("Rccmatch.cpp")
-   
-    admixture.case <- admixture[fam[,6] == case.string,]
-    admixture.control <- admixture[fam[,6] != case.string & !is.na(fam[,6]),]
-    admixture.dist <- dist(admixture.control, admixture.case)
 
-    ret <- ccmatch(admixture.dist, n)
-    return (ret)
+    x.case <- x[fam[,6] == case.string,]
+    if (nrow(x.case) == 0) stop("no case found.")
+    x.control <- x[fam[,6] != case.string & !is.na(fam[,6]),]
+    if (nrow(x.control) == 0) stop("no control found")
+    if (n < nrow(x.control) / nrow(x.case)) stop("ratio of case:control is less than n")
+    x.dist <- dist(x.control, x.case)
+
+    ret <- ccmatch(x.dist, n)
+    return (force(ret))
 }
 
-Rccmatch.naive <- function(x, N) {
-    df <- cbind(index = seq(ncol(x)), t(x))
-    matched <- rep(FALSE, ncol(x))
+Rccmatch.naive <- function(x, fam, n, case.string = "2") {
+    x.case <- x[fam[,6] == case.string,]
+    if (nrow(x.case) == 0) stop("no case found.")
+    x.control <- x[fam[,6] != case.string & !is.na(fam[,6]),]
+    if (nrow(x.control) == 0) stop("no control found")
+    if (n < nrow(x.control) / nrow(x.case)) stop("ratio of case:control is less than n")
+    x.dist <- dist(x.control, x.case)
+
+    df <- cbind(index = seq(ncol(x.dist)), t(x.dist))
+    matched <- rep(FALSE, ncol(x.dist))
     ret <- c()
     for (i in 2:ncol(df)) {
-        sorted <- df[order(df[,i]),]    
+        sorted <- df[order(df[,i]),]
         pair <- c(i - 1)
         sum = 0
         for (j in 1:nrow(df)) {
@@ -24,16 +34,16 @@ Rccmatch.naive <- function(x, N) {
                 pair <- c(pair, sorted[j, 1])
                 matched[sorted[j, 1]] = TRUE
                 sum = sum + sorted[j, i]
-                if (length(pair) == N + 1) {
+                if (length(pair) == n + 1) {
                     pair <- c(pair, sum)
                     break
-                }          
-            }      
+                }
+            }
         }
         ret <- rbind(ret, pair)
     }
     ret <- data.frame(ret)
-    colnames(ret) <- c("Case", paste("Control", seq(N), sep = ""), "Distance")
+    colnames(ret) <- c("Case", paste("Control", seq(n), sep = ""), "Distance")
     rownames(ret) <- seq(nrow(ret))
     return (ret)
 }
@@ -43,7 +53,7 @@ if (!interactive()) {
     if (length(args) >= 3) {
         admixture <- read.table(args[1], header = F, as.is = T)
         fam <- read.table(args[2], header = F, as.is = T, na.strings = "-9")
-    
+
         case.string <- "2"
         if (length(args) == 4) case.string = args[4]
 
