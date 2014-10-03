09584bedf05de2b3ead7205dc68296fe597c9038
Masahiro Kanai <mkanai.brc@tmd.ac.jp>
2014-10-03 14:36:00 +0900

    update

--- annovar/aggregate_anno.r
+++ annovar/aggregate_anno.r
@@ -4,5 +4,8 @@ output <- args[2]
 
 bim <- read.table(input, as.is = T, na.strings = "-9")
 agg <- aggregate(V2 ~ V7, data = bim, function(x) {paste(x, collapse = " ")})
-write.table(data.frame(agg), file = args[2], row.names = F, col.names = F, quote = F, sep = "\t")
+agg.pos <- aggregate(V4 ~ V7, data = bim, function(x) {paste(min(x), max(x), sep = "\t")})
+agg.chr <- aggregate(V1 ~ V7, data = bim, unique)
+df <- data.frame(agg[,1], agg.chr[,2], agg.pos[,2], agg[,2])
+write.table(df, file = args[2], row.names = F, col.names = F, quote = F, sep = "\t")
 
--- annovar/annotate_bim.sh
+++ annovar/annotate_bim.sh
@@ -13,11 +13,18 @@ bim_name=${bim##*/}
 
 cut -f11 $avinput > /tmp/$avinput_name
 csvquote $csv | cut -d',' -f7 | csvquote -u | sed -e '1d' -e 's/\"//g' > /tmp/$csv_name
-paste /tmp/$csv_name /tmp/$avinput_name | sort -k2 | uniq > ${csv_name%.csv}.gene
+paste /tmp/$csv_name /tmp/$avinput_name | sort -k2 | uniq > /tmp/${csv_name%.csv}.gene
 nrow="$(wc -l $bim | cut -d' ' -f1)"
 seq $nrow | paste $bim - | sort -k2 > /tmp/$bim_name
-join -j2 /tmp/$bim_name ${csv_name%.csv}.gene > /tmp/${bim_name}.anno
+join -j2 /tmp/$bim_name /tmp/${csv_name%.csv}.gene > /tmp/${bim_name}.anno
 sort -k7 -n /tmp/${bim_name}.anno | awk '{OFS="\t"}{print $2,$1,$3,$4,$5,$6,$8}' > ${bim_name}.anno
 
 Rscript $binary_path/aggregate_anno.r ${bim_name}.anno ${bim_name}.gene
+
+nrow2="$(wc -l ${bim_name}.anno | cut -d' ' -f1)"
+if [ $nrow -ne $nrow2 ]
+then
+    echo "Warning: number of SNPs are different"
+    cut -f1-6 ${bim_name}.anno | diff $bim -
+fi
 
