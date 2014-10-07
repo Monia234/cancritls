db9ed6bcc8fe27edfc3dce4e6b8bb6ee567c119f
Masahiro Kanai <mkanai.brc@tmd.ac.jp>
2014-10-07 20:18:06 +0900

    add usage

--- skat/makeSetID.sh
+++ skat/makeSetID.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
+function usage()
+{
+    cat << __EOS__
+    makeSetID.sh: make a .SetID file from .anno file for "SKAT" R package.
+
+usage: makeSetID.sh annofile
+
+__EOS__
+}
+
+if [ $# -ne 1 ]
+then
+    usage
+    exit 1
+fi
+
+binary_path=$(dirname $0)
 anno=$1
 filename=${anno%.bim.anno}
 filename=${filename##*/}
 
 awk '{OFS="\t"}{print $7, $2}' $anno > /tmp/$filename.SetID
-Rscript digestID.r /tmp/$filename.SetID $filename.SetID $filename.SetID.digestbl
+Rscript $binary_path/digestID.r /tmp/$filename.SetID $filename.SetID $filename.SetID.digestbl
 
