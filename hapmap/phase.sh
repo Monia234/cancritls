943d8ca0416037f116fcfea1152b3d1b3a51a473
Masahiro Kanai <mkanai.brc@tmd.ac.jp>
2014-11-19 20:32:35 +0900

    add phasing script

---
+++ hapmap/phase.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+hapmapdir=${1%/}
+
+for f in `ls $hapmapdir`
+do
+    if [ ${f##*.} == "dat" ]
+    then
+        filename=${f%.dat}
+        echo $filename
+        mach -d $hapmapdir/$filename.dat -p $hapmapdir/$filename.ped --rounds 50 --states 200 --phase
+    fi
+done
