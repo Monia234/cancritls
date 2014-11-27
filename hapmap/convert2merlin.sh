714a624a5184b9e44654ead1d05a94a7e1865453
Masahiro Kanai <mkanai.brc@tmd.ac.jp>
2014-11-27 18:42:54 +0900

    add usage

--- hapmap/convert2merlin.sh
+++ hapmap/convert2merlin.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
+function usage()
+{
+    cat << __EOS__
+convert2merlin.sh: convert a given HapMap dataset into MERLIN format (ped/dat/map).
+
+usage: convert2merlin.sh /path/to/hapmap /path/to/template
+__EOS__
+}
+
 hapmapdir=${1%/*}
 template=$2
 
-for i in `ls $hapmapdir`
+if [ $# -lt 2 ]
+then
+    usage
+    exit 1
+fi
+
+for i in `ls -F $hapmapdir | grep -v /`
 do
     filename=${i%.txt}
     echo $filename
