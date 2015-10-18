1fdb374ecc00a1c29ad170497880d2179c8393f9
Masahiro Kanai <mkanai.brc@tmd.ac.jp>
2015-10-18 21:22:17 +0900

    update smartpca.sh

--- eigenstrat/smartpca.sh
+++ eigenstrat/smartpca.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 file=${1%.bed}
-filename=${file##*/}
+filename=$(basename $file)
 numoutevec=${2:-'10'}
 
-smartpca.perl -i ${file}.bed -a ${file}.bim -b ${file}.fam -k ${numoutevec} -o ${filename}.pca -p ${filename}.plot -e ${filename}.eval -l ${filename}.log
+smartpca.perl -i ${file}.bed -a ${file}.bim -b ${file}.fam -k ${numoutevec} -o ${filename}.pca -p ${filename}.plot -e ${filename}.eval -l ${filename}.smartpca.log
 
